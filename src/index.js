b830f7d9e63a72909a4ab92629a561dcc92795b9
Adrian <adrianic92@yahoo.com>
2022-10-04 18:25:44 -0700

    commit tree ba30657b98bdfd8e7022808d0b99bf0a0ba15e46

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 let addToy = false;
+const collection = document.getElementById("toy-collection")
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
+const form = document.querySelector("form");
+let toyArray
+
+const toyURL = "http://localhost:3000/toys"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +18,88 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetchToys()
+
+  form.addEventListener("submit", newToy)
 });
+
+function renderToys(toys) {
+  toys.forEach(toy => renderToy(toy))
+}
+
+function renderToy(toy) {
+  const div = document.createElement("div")
+  div.className = "card"
+
+  const h2 = document.createElement("h2")
+  h2.innerText = toy.name
+
+  const img = document.createElement("img")
+  img.src = toy.image
+  img.className = "toy-avatar"
+
+  const p = document.createElement("p")
+  p.innerHTML = `${toy.likes} Likes`
+  p.id = `${toy.id}-like`
+
+  const button = document.createElement("button")
+  button.className = "like-btn"
+  button.id = toy.id
+  button.innerText = "Likes ❤️"
+  button.addEventListener("click", liker)
+
+  div.append(h2, img, p, button)
+  collection.append(div)
+}
+
+function newToy(e) {
+  e.preventDefault()
+  const nameEntry = e.target.name.value
+  const imageEntry = e.target.image.value
+
+  fetch(toyURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept" : "application/json"
+    },
+    body: JSON.stringify({
+      "name": nameEntry,
+      "image": imageEntry,
+      "likes": 0
+    })
+  })
+  .then(resp => resp.json())
+  .then(toy => {
+    renderToy(toy)
+    toyArray = toy
+  })
+}
+
+function liker(e) {
+  let toyID = e.target.id
+    fetch(`http://localhost:3000/toys/${toyID}`, {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": toyArray[toyID-1].likes + 1
+    })
+  })
+  .then(resp => resp.json())
+  .then(() => {
+    toyArray[toyID-1].likes++
+    document.getElementById(`${toyID}-like`).innerHTML = `${toyArray[toyID-1].likes} Likes`
+  })
+}
+
+function fetchToys() {
+  fetch(toyURL)
+  .then(resp => resp.json())
+  .then(data => {
+    toyArray = data
+    renderToys(toyArray)
+  })
+}
